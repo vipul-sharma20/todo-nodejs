b4c084bfc5f80ccc19a181aacef2a82ebf9d4b16
Vipul Sharma <vipul.sharma20@gmail.com>
2016-02-13 00:09:52 +0530

    added basic app operations

---
+++ routes/index.js
@@ -0,0 +1,50 @@
+
+/*
+ * GET home page.
+ */
+
+var mongoose = require( 'mongoose' );
+var Todo     = mongoose.model( 'Todo' );
+
+exports.index = function(req, res){
+  res.render('index', { title: 'TODO app' });
+};
+
+exports.create = function ( req, res ){
+  new Todo({
+    content    : req.body.content,
+    updated_at : Date.now()
+  }).save( function( err, todo, count ){
+    res.redirect( '/' );
+  });
+};
+
+// query db for all todo items
+exports.index = function ( req, res ){
+  Todo.find( function ( err, todos, count ){
+    res.render( 'index', {
+      title : 'Todo',
+      todos : todos
+    });
+  });
+};
+
+// remove todo item by its id
+exports.destroy = function ( req, res ){
+  Todo.findById( req.params.id, function ( err, todo ){
+    todo.remove( function ( err, todo ){
+      res.redirect( '/' );
+    });
+  });
+};
+
+// redirect to index when finish
+exports.update = function ( req, res ){
+  Todo.findById( req.params.id, function ( err, todo ){
+    todo.content    = req.body.content;
+    todo.updated_at = Date.now();
+    todo.save( function ( err, todo, count ){
+      res.redirect( '/' );
+    });
+  });
+};
